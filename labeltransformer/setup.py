05adf5cc632f5c98d18b461c099f9bcd1866665b
Rohith Ravindranath <rohithravin@gmail.com>
2019-07-18 14:42:50 -0700

    a

--- labeltransformer/setup.py
+++ labeltransformer/setup.py
@@ -1,16 +1,20 @@
-from setuptools import setup
+import setuptools
 
-setup(name='labeltransformer',
+setuptools.setup(name='labeltransformer',
       version='0.1',
       description='Label Transformer For Home Sale Model',
-      url='https://github.com/rohithravin/MachineLearningWithWatsonAndDB2/tree/master/labeltransformer',
+      url='https://github.com/rohithravin/MachineLearningWithWatsonAndDB2/blob/master/',
       author='Rohith Ravindranath',
       author_email='rohithravin@example.com',
       license='MIT',
-      packages=['labeltransformer'],
+      packages=setuptools.find_packages(),
       install_requires=[
           'pandas',
           'numpy',
           'sklearn'
-      ],
-      zip_safe=False)
+      ],classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    )
