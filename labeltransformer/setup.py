34892eeaf75f8692e7aac6ad50092337c4cbaf8c
Rohith Ravindranath <rohithravin@gmail.com>
2019-07-18 14:16:31 -0700

    update

---
+++ labeltransformer/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='labeltransformer',
+      version='0.1',
+      description='Label Transformer For Home Sale Model',
+      url='http://github.com/storborg/funniest',
+      author='Rohith Ravindranath',
+      author_email='rohithravin@example.com',
+      license='MIT',
+      packages=['labeltransformer'],
+      zip_safe=False)
